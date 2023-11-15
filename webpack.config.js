5322124ce2821b2353dd3d0ecddf6878a3ed611b
jeremy <nashoba.jeremy@gmail.com>
2023-11-15 14:12:44 +0100

    Changes by ttst

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ Encore
      */
     .addEntry('app', './assets/app.js')
     .addEntry('register', './assets/app.js')
+    .addEntry('post', './assets/post.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
