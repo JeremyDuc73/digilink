9b6f20186a77d5af7a85f6388aca089baf5dce18
jeremy <nashoba.jeremy@gmail.com>
2023-11-15 16:42:54 +0100

    Changes by ubuntu

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Comment;
 use App\Entity\Post;
 use App\Entity\Report;
+use App\Repository\ProfileRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,6 +17,7 @@ class ReportController extends AbstractController
     #[Route('/report/post/{id}', name: 'app_report_post')]
     public function reportPost(Post $post, EntityManagerInterface $manager): Response
     {
+
         if ($this->getUser() != null){
             $report = new Report();
                     $report->setAuthor($this->getUser()->getProfile());
