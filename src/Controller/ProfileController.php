0e69f06beaab00fde69619c41cc9bebca758a076
jeremy <nashoba.jeremy@gmail.com>
2023-11-15 16:42:50 +0100

    Changes by thomas

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Link;
 use App\Entity\Profile;
 use App\Form\ProfileType;
 use Doctrine\ORM\EntityManagerInterface;
@@ -15,20 +16,78 @@ class ProfileController extends AbstractController
     #[Route('/profile/{id}', name: 'app_profile')]
     public function index(Profile $profile): Response
     {
+        $blackList = $profile->getBlackList();
         return $this->render('profile/index.html.twig', [
             "profile"=>$profile,
+            "blackList"=>$blackList
+        ]);
+    }
+
+    #[Route('/profile/edit/{id}', name: 'app_profile_edit')]
+    public function edit(Profile $profile, EntityManagerInterface $manager, Request $request): Response
+    {
+        if (!$this->getUser() === $profile->getOfUser()) {return $this->redirectToRoute('app_home');}
+
+        $form = $this->createForm(ProfileType::class, $profile);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $profile->setDescription($profile->getDescription());
+            $profile->setGrade($profile->getGrade());
+            $profile->setPhoneNumber($profile->getPhoneNumber());
+
+            $links = $form->getData()->getLink();
+
+            foreach ($links as $link) {
+                $newLink = new Link();
+                $newLink ->setLinkName($link->getLinkName());
+                $newLink ->setUrl($link->getUrl());
+                $newLink ->setProfile($profile);
+            }
+
+
+            $manager->flush();
+            return $this->redirectToRoute('app_profile', ['id'=>$profile->getId()]);
+        }
+
+        return $this->render('profile/edit.html.twig', [
+            'profile' => $profile,
+            'form' => $form,
         ]);
     }
 
     #[Route('/blockuser/{id}', name: 'app_profile_blockuser')]
-    public function blockUser(Profile $profile){
+    public function blockUser(Profile $profile, EntityManagerInterface $manager){
         $curUser = $this->getUser()->getProfile();
         if ($profile != $curUser){
             $curUser->addBlackList($profile);
-            return $this->redirectToRoute("app_home");
+            $manager->persist($profile);
+            $manager->flush();
+            return $this->redirectToRoute("app_profile", [
+                'id'=>$curUser->getId()
+            ]);
         }
-        return $this->redirectToRoute("app_home");
+        return $this->redirectToRoute("app_profile", [
+            'id'=>$curUser->getId()
+        ]);
     }
+
+
+    #[Route('/unblockuser/{id}', name: 'app_profile_unblockuser')]
+    public function unblockUser(Profile $profile, EntityManagerInterface $manager){
+        $curUser = $this->getUser()->getProfile();
+
+        if  ($curUser->hasBlocked($profile)){
+            $curUser->removeBlackList($profile);
+            $manager->persist($curUser);
+            $manager->flush();
+        }
+
+        return $this->redirectToRoute("app_profile", [
+            'id'=>$curUser->getId()
+        ]);    }
+
 
 
 }
--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -6,6 +6,7 @@ use App\Entity\Grade;
 use App\Entity\Profile;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,6 +17,19 @@ class ProfileType extends AbstractType
         $builder
             ->add('description')
             ->add('phoneNumber')
+            ->add('grade', EntityType::class, [
+                'class'=>Grade::class,
+                'choice_label'=>'gradeName'
+            ])
+            ->add('link', CollectionType::class, [
+                'entry_type'=>LinkType::class,
+                'allow_add'=>true,
+                'allow_delete'=>true,
+                'required'=>true,
+                'by_reference'=>false,
+                'disabled'=>false,
+                'prototype'=>true
+            ])
         ;
     }
 
