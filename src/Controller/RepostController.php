b97cf7c4519a2472dd302d1b0a080d7208dfb38b
jeremy <nashoba.jeremy@gmail.com>
2023-11-15 16:20:51 +0100

    MERCREDI APREM

--- src/Controller/RepostController.php
+++ src/Controller/RepostController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Entity\Profile;
 use App\Entity\Repost;
 use App\Repository\RepostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,32 +15,51 @@ use Symfony\Component\Routing\Annotation\Route;
 class RepostController extends AbstractController
 {
     # ROUTE A METTRE APRES DANS PROFILECONTROLLER ET MERGE AVEC INDEX-POSTS
-    #[Route('s', name: 'app_reposts')]
-    public function allReposts(RepostRepository $repository): Response
+    #[Route('s/{id}', name: 'app_my_reposts')]
+    public function myReposts(Profile $profile): Response
     {
-        $reposts = $repository->findAll();
+        $reposts = $profile->getReposts();
         return $this->render('repost/index.html.twig', [
             'reposts'=>$reposts
         ]);
     }
 
     #[Route('/{id}', name: 'repost')]
-    public function repost(Post $post): Response
+    public function repost(Post $post, EntityManagerInterface $manager): Response
     {
         $alreadyReposted = $this->getUser()->getProfile()->getReposts();
-        foreach($alreadyReposted as $reposted){
-            if($reposted->getOriginalPost() == $post){
-                // mettre flash message pour dire que non
-                return $this->redirectToRoute('app_posts'); # redirection vers la page d'accueil ?
-            }
+        if ($alreadyReposted){
+            foreach($alreadyReposted as $reposted){
+                        if($reposted->getOriginalPost() == $post){
+                            // mettre flash message pour dire que non
+
+                            $this->addFlash(
+                                'notice',
+                                'You have already reposted this post. once is enough'
+                            );
+
+                            return $this->redirectToRoute('app_posts');
+                        }
+        }
+
+
 
             $repost = new Repost();
             $repost->setOriginalPost($post);
             $repost->setCreatedAt(new \DateTimeImmutable());
             $repost->setRepostedBy($this->getUser()->getProfile());
 
+            $manager->persist($repost);
+            $manager->flush();
+
+            $this->addFlash(
+                'notice',
+                'Reposted in your feed'
+            );
+
             return $this->redirectToRoute('app_posts'); # redirection vers la page d'accueil ?
         }
+
         return $this->redirectToRoute('app_posts'); # redirection vers la page d'accueil ?
 
 
